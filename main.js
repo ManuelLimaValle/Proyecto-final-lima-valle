605722e14c023c23cf66b466fbf120f4b31e603c
Alex Lima Valle <manuel24051999@gmail.com>
2022-09-17 22:54:02 -0300

    correcciones

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+//***************************************************************CURRUSEL********************************** */
+
+
+
+
+
+
+
+
+// ************************************************* NOTEBOOK **************************************************
+let cartShopping = [];
 
 class Computadora{
     constructor(name,modelo,mark,color,ram,processor,graphics,screen,disc,precio){
@@ -14,81 +25,87 @@ class Computadora{
     }
 }
 
-const lenovoIdeapadFlex5 = new Computadora("Ideapad flex 5","1165G7","LENOVO","Gris",16,"intel core i7","INTEL IRIS",15.6,512,195000);
+const lenovoIdeapadFlex5 = new Computadora("Ideapad flex 5","1165G7","LENOVO","Gris",16,"intel core i7","INTEL IRIS",15.6,"512 GB",195000);
 
-const hpPavilion = new Computadora("Hp Pavilion","13BB9X","Hp","Blanco",8,"intel core i7","INTEL IRIS",13.3,512,190000);
+const hpPavilion = new Computadora("Hp Pavilion","13BB9X","HP","Blanco",8,"intel core i7","INTEL IRIS",13.3,"512 GB",190000);
 
-const msiThin = new Computadora("Msi Thin","GF65THIN","MSI","Negro",16,"intel i7 10TH GEN","GEFORCE GTX 6GB 1660",15.6,512,270000);
+const msiThin = new Computadora("Msi Thin","GF65THIN","MSI","Negro",16,"intel i7 10TH GEN","GEFORCE GTX 6GB 1660",15.6,"512 GB",270000);
 
-const hpEnvy = new Computadora("Hp Envy","15-EP1065","HP","Blanco",32,"intel core i7","GEFORCE RTX 3060",15.6,1,380000);
+const hpEnvy = new Computadora("Hp Envy","15-EP1065","HP","Blanco",32,"intel core i7","GEFORCE RTX 3060",15.6,"1 TB",380000);
 
-const lenovoLegionY7 = new Computadora(" LEGIÓN Y740","Y740","LENOVO","Negro",32,"intel core i7 9th Gen","GEFORCE RTX 8GB 2070 ",15.6,256,395000);
+const lenovoLegionY7 = new Computadora("LENOVO LEGIÓN Y740","Y740","LENOVO","Negro",32,"intel core i7 9th Gen","GEFORCE RTX 8GB 2070",15.6,"256 GB",395000);
+
+const asusVivobookX7 = new Computadora("ASUS VIVOBOOK","X712JA","ASUS","Blanco",20,"intel core i5 10th Gen","INTEL IRIS",17.3,"256 GB",180000);
+
+const lenovoIdeapad14 = new Computadora("LENOVO IDEAPAD","14IML05","LENOVO","Blanco",20,"intel core i5 10th Gen","INTEL IRIS",14,"512 GB",155000);
+
+const msiStealth15M = new Computadora("MSI STEALTH 15M A11UEKV","A11UEKV","MSI","Gris",32,"intel core i7","GEFORCE RTX 3060 6GB",15.6,"1 TB",415000);
 
 const mainDiv = document.querySelector(".container");
 
-let computers = [lenovoIdeapadFlex5,hpPavilion,msiThin,hpEnvy,lenovoLegionY7];
+let computers = [lenovoIdeapadFlex5,hpPavilion,msiThin,hpEnvy,lenovoLegionY7,asusVivobookX7,lenovoIdeapad14,msiStealth15M];
+
 const fragmen = document.createDocumentFragment();
-let carrito = [];
-
-function createContainer(notebook){
-    let container = document.createElement("DIV");
-    container.classList.add("container__div")
-    let img = document.createElement("IMG");
-    img.setAttribute("src",`../img/notebook/${notebook.modelo}.png`);
-    img.classList.add(`container__img`);
-    createList(container,notebook);
-    let button = document.createElement("BUTTON");
-    button.innerHTML="agregar a carrito";
-    button.classList.add("container__button")
-    button.addEventListener("click",()=>{addCart(notebook);})
-    let price = document.createElement("P");
-    price.innerHTML = `${notebook.precio} $`
-    price.classList.add("container__p");
-    container.appendChild(price);
-    container.appendChild(img);
-    container.appendChild(button);
-    fragmen.appendChild(container);
-}
-
-function createList(computer,note){
-        let list = document.createElement("UL");
-        list.classList.add("container__ul");
-        for(let i=0;i<6;i++){
-            let items = document.createElement("LI");
-            items.classList.add("container__li");
-            list.appendChild(items);
-        }
-        list.children[0].innerHTML = `<h2>${note.name}</h2>`;
-        list.children[1].innerHTML = `${imagen('ssd','png')}<span class="container__span">${note.disc} GB</span>`;
-        list.children[2].innerHTML = `${imagen('ram','png')}<span class="container__span">${note.ram} GB</span>`;
-        list.children[3].innerHTML = `${imagen('widescreen','png')}<span class="container__span">${note.screen}</span>`;
-        list.children[4].innerHTML = `${imagen(`${note.processor}`,'jpg')}<span class="container__span">${note.processor}</span>`;
-        list.children[5].innerHTML = `${imagen(note.graphics.substring(0,11),'jpg')}<span class="container__span">${note.graphics}</span>`
-        computer.appendChild(list);
-}
-
-function addCart(item){
-    carrito.push(item);
-    console.log(carrito);
-}
 
 const imagen = (item1,item2) =>{ return `<img src ="./img/logos/${item1}.${item2}" class="container__logo">`}
 
-for(let laptop of computers){
-    createContainer(laptop);
+
+computers.forEach((produc)=>{
+    const container = document.createElement("DIV");
+    container.classList.add("container__div")
+    container.innerHTML =`
+    <ul class="container__ul">
+        <li class="container__li">
+            <h4 class="container__h2">${produc.name}</h4>
+        </li>
+        <li class="container__li">
+            <img src="./img/logos/ssd.png" class="container__logo">
+            <span class="container__span">${produc.disc} </span>
+        </li>
+        <li class="container__li">
+            <img src="./img/logos/ram.png" class="container__logo">
+            <span class="container__span">${produc.ram} GB </span>
+        </li>
+        <li class="container__li">
+            <img src="./img/logos/widescreen.png" class="container__logo">
+            <span class="container__span">${produc.screen}</span>
+        </li>
+        <li class="container__li">
+            <img src="./img/logos/${produc.processor}.jpg" class="container__logo">
+            <span class="container__span">${produc.processor} </span>
+        </li>
+        <li class="container__li">
+            <img src="./img/logos/${produc.graphics.substring(0,11)}.jpg" class="container__logo">
+            <span class="container__span">${produc.graphics} </span>
+        </li>
+    </ul>
+    <p class="container__p">${produc.precio}$</p>
+    <img src="../img/notebook/${produc.modelo}.png" class="container__img" alt="${produc.modelo}">
+    <button class="container__button" id="${produc.modelo}">agregar a carrito</button>
+    `;
+    fragmen.appendChild(container);
+    mainDiv.appendChild(fragmen);
+    const botton = document.getElementById(produc.modelo);
+    botton.addEventListener("click",()=>{
+        addCard(produc.modelo);
+    })
+});
+
+const addCard = (idType)=>{
+    const item = computers.find((e) => {e.modelo == idType})
+    cartShopping.push(item);
+    number.innerText = cartShopping.length;
+    console.log(item);
 }
 
-mainDiv.appendChild(fragmen);
+
 
 const navbar = document.querySelector(".nav__menu");
 const menu = document.querySelector(".nav__ul")
 
 navbar.addEventListener("click",() => {menu.classList.toggle("nav__active")});
 
-console.log(carrito.length)
-
-
 let number = document.querySelector(".nav__number");
-number.innerHTML = carrito.length;
+number.innerHTML = cartShopping.length;
 
 
